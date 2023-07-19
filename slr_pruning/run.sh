6a8fd7006b1ef41e9ca699f6eb3951f431cf0f40
Zhoushanglin100 <zhoushanglin100@gmail.com>
2023-07-19 13:40:44 -0700

    Update run.sh

--- slr_pruning/run.sh
+++ slr_pruning/run.sh
@@ -1,16 +1,27 @@
-### Step 1: run SLR train
+export optim=savlr     # choose from [savlr, admm]
+export model=18
+export ratio=0.7
+export bz=128
+export ext=_tmp1
+
+### Step 1: run SLR / ADMM train
 ## change @config-file based on needed
-python3 main.py --optimization savlr --admm-train --config-file config_resnet18_0.9 --batch-size 128
+CUDA_VISIBLE_DEVICES=$1 python3 main.py \
+            --optimization $optim \
+            --arch resnet$model \
+            --admm-train \
+            --config-file config_resnet$model\_$ratio \
+            --baseline-model ckpts/baseline/resnet$model.pt \
+            --batch-size $bz \
+            --ext $ext
 
 ### Step 2: run maked retrain
 ## change @config-file based on needed
-## change @admmtrain-acc based on model from admm train
-python3 main.py --optimization savlr --masked-retrain --admmtrain-acc 76.08 --config-file config_resnet18_0.9 --batch-size 128 --ext _tmp3
+CUDA_VISIBLE_DEVICES=$1 python3 main.py \
+            --optimization $optim \
+            --arch resnet$model \
+            --masked-retrain \
+            --config-file config_resnet$model\_$ratio \
+            --batch-size $bz \
+            --ext $ext
 
-
-
-### Step 1: run ADMM train
-python3 main.py --optimization admm --admm-train --config-file config_resnet18_0.9 --batch-size 128
-
-### Step 2: run maked retrain
-python3 main.py --optimization admm --masked-retrain --admmtrain-acc 76.08 --config-file config_resnet18_0.9 --batch-size 128 --ext _tmp3
